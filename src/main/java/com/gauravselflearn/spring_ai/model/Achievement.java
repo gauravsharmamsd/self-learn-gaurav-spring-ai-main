4cd48b919cd1bc3c154ad00fb24f561f0db1a872
Gaurav Kaushik <gauravsharmamsd.com>
2025-03-29 08:04:45 +0530

    initial commmit 1

---
+++ src/main/java/com/gauravselflearn/spring_ai/model/Achievement.java
@@ -0,0 +1,4 @@
+package com.gauravselflearn.spring_ai.model;
+
+public record Achievement(String achievement) {
+}
---
+++ src/main/java/com/gauravselflearn/spring_ai/model/Player.java
@@ -0,0 +1,6 @@
+package com.gauravselflearn.spring_ai.model;
+
+import java.util.List;
+
+public record Player(String playerName, List<String> achievements) {
+}
